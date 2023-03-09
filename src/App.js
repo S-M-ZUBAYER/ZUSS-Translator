a4918829c0a049f2a3c9335faf39cb9ac76cef39
S-M-ZUBAYER <smzubayer18@gmail.com>
2023-03-09 11:10:02 +0600

    Added necessary comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { AiOutlineCopy, AiOutlineSound } from 'react-icons/ai';
 import { CiMicrophoneOn } from 'react-icons/ci';
@@ -7,137 +6,85 @@ import countries from './Countries';
 
 function App() {
 
-  const [selectLan, setSelectLan] = useState("")
   const [targetLan, setTargetLan] = useState("")
   const [text, setText] = useState('');
   const [midTranslate, setMidTranslate] = useState('');
   const [targetTranslate, setTargetTranslate] = useState('');
-  
-  const toText=document.getElementById("to-text");
-  const midText=document.getElementById("mid-text");
 
+  const toText = document.getElementById("to-text");
+  const midText = document.getElementById("mid-text");
 
-const handleTargetLan = (event) => {
+ // create the function to collect the target language to translate
+  const handleTargetLan = (event) => {
     event.preventDefault();
     setTargetLan(event.target.value)
 
   }
 
+  // create the function to collect the input text from the textarea to translate
   const handleToCollectText = (event) => {
     event.preventDefault();
     setText(event.target.value)
 
   }
 
-  const detectLan = (event) => {
-    event.preventDefault();
-    setSelectLan(event.target.value)
-
-  }
-  
-// console.log(targetLan)
 
   const handleToTranslate = () => {
 
-toText.value="Translating"
-midText.value="Translating"
+    // toText.value = "Translating"
+    setTargetTranslate("Translating");
+    setMidTranslate("Translating");
 
- setText( text.trim());
+    setText(text.trim());
 
- const midInput={
-  text
-        }
- const targetInput={
-  target:targetLan,
-  text
-        }
-
-
-
-//  let apiUrl = `http://localhost:5000/translate`;
-//       fetch(apiUrl,{
-//         method:"POST",
-//         headers:{
-//           "content-type":"application/json"
-//         },
-//         body:JSON.stringify(targetInput)
-//       })
-//         .then((res) => res.json())
-//         .then((data) => {
-//           // console.log(data.data)
-//           // toText.value = data.responseData.translatedText;
-//           // console.log(data?.isAxiosError)
-//           setTargetTranslate(data?.data);
-//           // finalText.innerText = finalTranslate;
-//           // // toText.value=finalTranslate;
-//           // const result=document.getElementById("outPut");
-//           // result.value=finalTranslate;
-//           // data.matches.forEach((data) => {
-//           //   if (data.id === 0) {
-//           //     toText.value = data.translation;
-//           //   }
-//           // });
-//           // toText.setAttribute("placeholder", "Translation");
-//         });
-
-        let apiUrlEng = `http://localhost:5000/translateEng`;
-
-      fetch(apiUrlEng,{
-        method:"POST",
-        headers:{
-          "content-type":"application/json"
-        },
-        body:JSON.stringify(midInput)
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          // console.log(data.data)
-          // toText.value = data.responseData.translatedText;
-          setMidTranslate(data?.data);
-          setText(data.data)
-          // finalText.innerText = finalTranslate;
-          // // toText.value=finalTranslate;
-          // const result=document.getElementById("outPut");
-          // result.value=finalTranslate;
-          // data.matches.forEach((data) => {
-          //   if (data.id === 0) {
-          //     toText.value = data.translation;
-          //   }
-          // });
-          // toText.setAttribute("placeholder", "Translation");
-        });
+    const midInput = {
+      text
+    }
+    const targetInput = {
+      target: targetLan,
+      text
+    }
 
 
-        let apiUrl = `http://localhost:5000/translate`;
-        fetch(apiUrl,{
-          method:"POST",
-          headers:{
-            "content-type":"application/json"
-          },
-          body:JSON.stringify(targetInput)
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            // console.log(data.data)
-            // toText.value = data.responseData.translatedText;
-            // console.log(data?.isAxiosError)
-            setTargetTranslate(data?.data);
-            // finalText.innerText = finalTranslate;
-            // // toText.value=finalTranslate;
-            // const result=document.getElementById("outPut");
-            // result.value=finalTranslate;
-            // data.matches.forEach((data) => {
-            //   if (data.id === 0) {
-            //     toText.value = data.translation;
-            //   }
-            // });
-            // toText.setAttribute("placeholder", "Translation");
-          });
+    // fetch the api to use post method the get the response from OpenAI
+    // Here we can get the value from detect language to English language
 
-        console.log(midTranslate,targetTranslate)
+    let apiUrlEng = `https://simple-node-server-s-m-zubayer.vercel.app/translateEng`;
 
-toText.value=targetTranslate
-midText.value=midTranslate
+    fetch(apiUrlEng, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(midInput)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMidTranslate(data?.data);
+        setText(data.data)
+      });
+
+
+    // fetch the api to use post method the get the response from OpenAI
+    // Here we can get the value from English language to selected language
+
+    let apiUrl = `https://simple-node-server-s-m-zubayer.vercel.app/translate`;
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(targetInput)
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTargetTranslate(data?.data);
+      });
+
+    console.log(midTranslate, targetTranslate)
+
+    toText.value = targetTranslate
+    midText.value = midTranslate
 
   }
 
@@ -145,46 +92,44 @@ midText.value=midTranslate
 
 
   return (
-    <div className="App md:h-screen flex justify-center items-center bg-lime-300">
 
+
+    <div className="App md:h-screen flex justify-center items-center bg-lime-300">
       <div className=" ">
-        <h1 className="text-3xl font-bold mb-10">ZUSS Translator</h1>
-        <div className=' bg-white md:flex rounded-lg p-5'>
+        <h1 className="text-3xl font-bold mb-10 mt-10 md:mt-0">ZUSS Translator</h1>
+       
+
+        <div className=' bg-white md:flex rounded-lg p-5'>  
           <div>
-            <textarea placeholder="please type here" onChange={handleToCollectText} className="border-2 border-slate-600 rounded-t-lg md:rounded-t-none md:rounded-tl-lg p-2  mt-2 md:ml-2 h-60  w-72  block"></textarea>
+            <textarea placeholder="please type here" onChange={handleToCollectText} className="border-2 border-slate-600 rounded-t-lg md:rounded-t-none md:rounded-tl-lg p-2  mt-2 md:ml-2 h-60  w-72 md:w-64 lg:w-72  block"></textarea>
             <div className="border-2 border-slate-600 md:ml-2 sm:rounded-b-lg md:rounded-b-none md:rounded-bl-lg mb-2 mr-2 md:mr-0">
-              <div className="flex justify-between items-center mx-2 ">
+              <div className="flex justify-between items-center ml-2 ">
                 <AiOutlineCopy></AiOutlineCopy>
                 <CiMicrophoneOn></CiMicrophoneOn>
                 <AiOutlineSound></AiOutlineSound>
-
-
                 <div className="form-control">
                   <div className="input-group" id='lan'>
-                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900">
-                      <option disabled selected> Select language</option>
-                      {
-                        countries?.map((element, index) => <option key={index}>{element}</option>
-
-                        )
-                      }
-
+                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-2 py-2 bg-white rounded-lg  focus:outline-green-500  text-gray-900">
+                      <option disabled selected> Detect language</option>
                     </select>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
+
+
           <div>
-            <textarea placeholder="Translation" id="mid-text"  className="border-2 rounded-t-lg  md:rounded-t-none border-slate-600 p-2  mt-2 h-60  w-72 block"></textarea>
+            <textarea placeholder="Translation" id="mid-text" className="border-2 rounded-t-lg  md:rounded-t-none border-slate-600 p-2  mt-2 h-60  w-72 md:w-64 lg:w-72 block"></textarea>
             <div className="border-2 border-slate-600  mb-2 mr-2 md:mr-0">
-              <div className="flex justify-between items-center mx-2 ">
+              <div className="flex justify-between items-center ml-2 ">
                 <AiOutlineCopy></AiOutlineCopy>
                 <CiMicrophoneOn></CiMicrophoneOn>
                 <AiOutlineSound></AiOutlineSound>
                 <div className="form-control">
                   <div className="input-group" id='lan'>
-                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900">
+                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-3 py-2 rounded-md  focus:outline-green-500 bg-white text-gray-900">
                       <option disabled selected>English</option>
 
                     </select>
@@ -193,16 +138,22 @@ midText.value=midTranslate
               </div>
             </div>
           </div>
+
+
+
           <div>
-            <textarea placeholder="Translation" id="to-text" className="border-2 border-slate-600 rounded-t-lg md:rounded-none md:rounded-tr-lg p-2 mt-2 mr-2 h-60  w-72 block"></textarea>
+            <textarea placeholder="Translation" id="to-text" className="border-2 border-slate-600 rounded-t-lg md:rounded-none md:rounded-tr-lg p-2 mt-2 mr-2 h-60  w-72 md:w-64 lg:w-72 block"></textarea>
             <div className="border-2 border-slate-600 md:rounded-br-lg mr-2  mb-2">
-              <div className="flex justify-between items-center mx-2 ">
+              <div className="flex justify-between items-center ml-2 ">
                 <AiOutlineCopy></AiOutlineCopy>
                 <CiMicrophoneOn></CiMicrophoneOn>
                 <AiOutlineSound></AiOutlineSound>
+
+                {/* To create the functionalities the select language */}
+
                 <div className="form-control" onChange={handleTargetLan}>
                   <div className="input-group" id='lan'>
-                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900">
+                    <select name='language' type="boolean" placeholder='Sold Status' className="select select-bordered w-full px-3 py-2 rounded-md focus:outline-green-500 bg-white text-gray-900">
                       <option disabled selected> Select language</option>
                       {
                         countries?.map((element, index) => <option key={index}>{element}</option>
@@ -216,10 +167,15 @@ midText.value=midTranslate
             </div>
           </div>
 
+
+
         </div>
+
+
+        {/* Create the button to generate the Translation */}
+
         <div className="flex justify-around items-center ">
           <button className="my-5 bg-white w-full py-1 rounded-lg font-semibold" onClick={handleToTranslate} >Text Translate</button>
-
         </div>
 
       </div>
